7b9c4b3ea6219be64d13db161627a55b3bc44e70
Affan Bin Imran - <affanbinimran.-@student.manchester.ac.uk>
2025-06-24 17:59:01 +0400

    add stream

--- next-app/app/api/streams/route.tsx
+++ next-app/app/api/streams/route.tsx
@@ -2,6 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import { prismaClient } from "@/app/lib/db";
 
+const YT_REGEX =
+  /^(?:https?:\/\/)?(?:www\.)?(?:m\.)?(?:youtube\.com\/(?:watch\?(?!.*\blist=)(?:.*&)?v=|embed\/|v\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})(?:[?&]\S+)?$/;
+
 const CreateStreamSchema = z.object({
   creatorId: z.string(),
   url: z.string().url(),
@@ -10,10 +13,21 @@ const CreateStreamSchema = z.object({
 export async function POST(req: NextRequest) {
   try {
     const data = CreateStreamSchema.parse(await req.json());
-    // prismaClient.stream.create({
-    //   userId: data.creatorId,
-    //   url: data.url,
-    // });
+
+    const isYT = YT_REGEX.test(data.url);
+
+    if (!isYT) {
+      return NextResponse.json({ error: "wrong url" }, { status: 411 });
+    }
+
+    await prismaClient.stream.create({
+      data: {
+        userId: data.creatorId,
+        url: data.url,
+        extractedId: data.url.split("?v=")[1],
+        type: "Youtube",
+      },
+    });
   } catch (error) {
     return NextResponse.json(
       { error: "error while adding stream" },
